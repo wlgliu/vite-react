11db3eb3de442a9698c09590e7f612eaaf0d3acb
wangleigang <wangleigang@funji.club>
2023-07-21 17:20:10 +0800

    config vite

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,11 +1,14 @@
 import { Button } from "antd";
+import { Tweet } from "react-twitter-widgets";
 
 export default function Home() {
 
   return (
-    <div className=" w-screen h-screen bg-red-300">
+    <div className=" w-screen h-screen">
         <div className=" container m-auto">
             <Button>Detail</Button>
+
+            <Tweet tweetId="1671298633872941056" />
         </div>
     </div>
   )
